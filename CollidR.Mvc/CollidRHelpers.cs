1d9d04dcab27877230cb37d2e627d71ed398b172
David Paquette <david.paquette@usask.ca>
2013-10-02 21:13:13 -0500

    Added HtmlHelper to add script to an MVC page for a model

---
+++ CollidR.Mvc/CollidRHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollidR.Mvc
+{
+    public static class CollidRHelpers
+    {
+        public static MvcHtmlString RegisterCollidR<TModel>(this HtmlHelper<TModel> helper, Expression<Func<TModel, int>> expression)
+        {
+            PropertyInfo propertyInfo = GetPropertyInfoFromExpression(expression);
+            StringBuilder scriptBuilder = new StringBuilder();
+
+            scriptBuilder.AppendLine(@"<script type='text/javascript'>");
+            scriptBuilder.AppendLine(@"var collidR = new $.collidR(");
+            scriptBuilder.AppendLine("{");
+            scriptBuilder.AppendFormat("entityType: '{0}',", typeof(TModel).FullName);
+            scriptBuilder.AppendLine();
+            scriptBuilder.AppendFormat("entityId: {0}", propertyInfo.GetValue(helper.ViewData.Model));
+            scriptBuilder.AppendLine();
+            scriptBuilder.AppendLine("})");
+            scriptBuilder.AppendLine(".registerClient();");
+
+            scriptBuilder.Append(@"</script>");
+            return new MvcHtmlString(scriptBuilder.ToString());
+        }
+
+        private static PropertyInfo GetPropertyInfoFromExpression<T>(Expression<Func<T, int>> expression)
+        {
+            MemberExpression member = expression.Body as MemberExpression;
+            if (member == null)
+            {
+                throw new ArgumentException(string.Format("Expression '{0}' refers to a method instead of a property.", expression));
+            }
+
+            return member.Member as PropertyInfo;
+
+        }
+    }
+}
--- CollidR.Sample/Controllers/AccountController.cs
+++ CollidR.Sample/Controllers/AccountController.cs
@@ -81,7 +81,7 @@ namespace CollidR.Sample.Controllers
                 {
                     WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
                     WebSecurity.Login(model.UserName, model.Password);
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Person");
                 }
                 catch (MembershipCreateUserException e)
                 {
@@ -337,7 +337,7 @@ namespace CollidR.Sample.Controllers
             }
             else
             {
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("Index", "Person");
             }
         }
 
